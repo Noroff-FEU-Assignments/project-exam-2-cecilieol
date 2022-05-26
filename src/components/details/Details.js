42fd90fbda19e30d3ebdbcc5e23f5a30a4a2bb10
cecilieol <cecilieolsen14@gmail.com>
2022-05-26 18:00:09 +0200

    Update Details.js

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,67 +1,17 @@
 import { useState, useEffect } from "react";
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
 import { useParams } from "react-router-dom";
-import { api } from "../../constants/api";
+import { api, hotelEndpoint } from "../../constants/api";
 import Carousel from "react-bootstrap/Carousel";
-import { Heading, Subheading } from "../layout/Heading";
 import Location from "./Location";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
-import axios from "axios";
-
-const schema = yup.object().shape({
-	first_name: yup
-		.string()
-		.required("Please enter your first name")
-		.min(2, "First name must be at least 2 characters"),
-	last_name: yup
-		.string()
-		.required("Please enter your first name")
-		.min(2, "Last name must be at least 2 characters"),
-	email: yup
-		.string()
-		.required("Please enter you email address")
-		.email('Invalid email format'),
-	guests: yup
-		.number()
-		.required("Please choose number of guests"),
-	checkin: yup
-		.date()
-		.required("Please choose a check-in date"),
-	checkout: yup
-		.date()
-		.required("Please choose a check-out date")
-		.min(yup.ref('checkin'), "Check-out date cannot be before check-in date"),
-	message: yup
-		.string(),
-		
-  });
+import EnquiryModal from "./Modal";
 
 export default function ViewPage() {
 	const [page, setPage] = useState(null);
 	const [fetchingPost, setFetchingPost] = useState(true);
 	const [fetchError, setFetchError] = useState(null);
-
-	const [show, setShow] = useState(false);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    const [submitted, setSubmitted] = useState(false);
-
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm({
-        resolver: yupResolver(schema)
-    });
 
 	let { id } = useParams();
 
@@ -69,7 +19,7 @@ export default function ViewPage() {
 		function () {
 			async function getPage() {
 				try {
-					const response = await fetch(api + "/" + id);
+					const response = await fetch(api + hotelEndpoint + "/" + id);
 					const json = await response.json();
 
 				    setPage(json);
@@ -93,38 +43,9 @@ export default function ViewPage() {
 
 	if (fetchError) return <div>Error loading post</div>;
 
-	async function onSubmit(data, e) {
-
-        await axios
-        .post('http://localhost:1337/api/enquiries?populate=%2A', {
-            data: {
-                hotel: id,
-                first_name: data.first_name,
-                last_name: data.last_name,
-                email: data.email,
-                guests: data.guests,
-                checkin: data.checkin,
-                checkout: data.checkout,
-                message: data.message,
-                }
-            },
-        )
-
-        .then(response => {
-            console.log('Well done!');
-            console.log('User profile', response.data);
-
-            setSubmitted(true);
-            e.target.reset();
-
-        })
-        .catch(error => { 
-            console.log('An error occurred:', error.response);
-        });
-    }
-
 	return (
 		<>
+			<div className="p-1"></div>
 			<Carousel variant="dark" interval={null}>
 			  <Carousel.Item>
 			    <img
@@ -133,7 +54,7 @@ export default function ViewPage() {
 			      alt={page.data.attributes.name}
 			    />
 			    <Carousel.Caption>
-			      <p>Image from {page.data.attributes.name}</p>
+			      <p>{page.data.attributes.name}</p>
 			    </Carousel.Caption>
 			  </Carousel.Item>
 			  <Carousel.Item>
@@ -143,7 +64,7 @@ export default function ViewPage() {
 			      alt={page.data.attributes.name}
 			    />
 			    <Carousel.Caption>
-			      <p>Image from {page.data.attributes.name}</p>
+			      <p>{page.data.attributes.name}</p>
 			    </Carousel.Caption>
 			  </Carousel.Item>
 			  <Carousel.Item>
@@ -153,14 +74,15 @@ export default function ViewPage() {
 			      alt={page.data.attributes.name}
 			    />
 			    <Carousel.Caption>
-			      <p>Image from {page.data.attributes.name}</p>
+			      <p>{page.data.attributes.name}</p>
 			    </Carousel.Caption>
 			  </Carousel.Item>
 			</Carousel>
+
 			<Container fluid className="details-container">
-			<Container fluid className="desktop-flex-between">
+			<Container fluid className="desktop-flex-between p-0">
 			<Row className="page-left">
-				<Heading title={page.data.attributes.name} />
+				<h1 className="h1-align-left">{page.data.attributes.name}</h1>
 				<p>{page.data.attributes.description}</p>
 			</Row>
 			<Card className="page-right">
@@ -172,70 +94,20 @@ export default function ViewPage() {
 					<Card.Text>Max # of guests: </Card.Text>
 					<Card.Text>{page.data.attributes.guests}</Card.Text>
 				</div>
-				<Button className="primary" onClick={handleShow}>Send enquiry</Button>
-  
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Send an enquiry to {page.data.attributes.name}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form onSubmit={handleSubmit(onSubmit)}>
-            {submitted && <div className="success">Your enquiry has been sent</div>}
-
-              <Form.Group className="mb-3 form-inline inline-first">
-                    <Form.Label>First name</Form.Label>
-                    <Form.Control type="text" name="first_name" placeholder="Your first name" {...register("first_name")}/>
-                    {errors.first_name && <span className="text-danger">{errors.first_name.message}</span>}
-                </Form.Group>
-
-                <Form.Group className="mb-3 form-inline">
-                    <Form.Label>Last name</Form.Label>
-                    <Form.Control type="text" name="last_name" placeholder="Your last name" {...register("last_name")}/>
-                    {errors.last_name && <span className="text-danger">{errors.last_name.message}</span>}
-                </Form.Group>
-
-                <Form.Group className="mb-3 form-inline inline-first">
-                    <Form.Label>E-mail address</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Your email" {...register("email")}/>
-                    {errors.email && <span className="text-danger">{errors.email.message}</span>}
-                </Form.Group>
-                <Form.Group className="mb-3 form-inline">
-                    <Form.Label>Number of guests</Form.Label>
-                    <Form.Control type="number" name="guests" placeholder="Number of guests" {...register("guests")}/>
-                    {errors.guests && <span className="text-danger">{errors.guests.message}</span>}
-                </Form.Group>
-
-                <Form.Group className="mb-3 form-inline inline-first">
-                    <Form.Label>Check-in</Form.Label>
-                    <Form.Control type="date" name="checkin" placeholder="Your checkin" {...register("checkin")} />
-                    {errors.checkin && <span className="text-danger">{errors.checkin.message}</span>}
-                </Form.Group>
-                <Form.Group className="mb-3 form-inline">
-                    <Form.Label>Check-out</Form.Label>
-                    <Form.Control type="date" name="checkout" placeholder="Your checkout" {...register("checkout")} />
-                    {errors.checkout && <span className="text-danger">{errors.checkout.message}</span>}
-                </Form.Group>
-
-                <Form.Group className="mb-3">
-                    <Form.Label>Additional message</Form.Label>
-                    <Form.Control as="textarea" rows={4} name="message" placeholder="Your message (optional)" {...register("message")}/>
-                    {errors.message && <span className="text-danger">{errors.message.message}</span>}
-                </Form.Group>
-
-                <Button type="submit" className="primary">Send enquiry</Button>
-                <Button className="secondary" onClick={handleClose}>Return</Button>
-
-            </Form>
-          </Modal.Body>
-        </Modal>
+			<EnquiryModal />
 			</Card>
 			</Container>
 
-			<Subheading title="Facilities" />
+			<h2 className="h2-details">Facilities</h2>
 				<ul className="facilities">
-					<li>{page.data.attributes.facilities}</li>
+					<li><i className="fa fa-wifi"></i> Wi-Fi: {page.data.attributes.wifi}</li>
+					<li><i className="fa fa-utensils"></i> Breakfast included: {page.data.attributes.breakfast_included}</li>
+					<li><i className="fa fa-bath"></i> Private bathroom: {page.data.attributes.private_bathroom}</li>
+					<li><i className="fa fa-wheelchair"></i> Disability friendly: {page.data.attributes.disability_friendly}</li>
+					<li><i className="fa fa-paw"></i> Pet friendly: {page.data.attributes.pet_friendly}</li>
+					<li><i className="fa fa-umbrella"></i> Complimentary Holidaze umbrellas: Yes</li>
 				</ul>
-			<Subheading title="Location" />
+			<h2 className="h2-details">Location</h2>
 			<p>{page.data.attributes.address}</p>
 			<div className="view-location">
     			<Location />
