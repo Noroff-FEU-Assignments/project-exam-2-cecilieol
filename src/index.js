8c860f1fd439db81853ad1ca8c7eccf8dddea870
cecilieol <cecilieolsen14@gmail.com>
2022-05-26 18:01:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './scss/style.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'font-awesome/css/font-awesome.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
