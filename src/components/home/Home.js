6ac514de0a77803285e9027abdcde4779a48a6e4
cecilieol <cecilieolsen14@gmail.com>
2022-05-29 13:33:07 +0200

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,6 @@
 import Row from "react-bootstrap/Row";
 import SearchResults from "./SearchResults";
-import FeaturedList from "./FeaturedList";
+import FeaturedList from "./list/FeaturedList";
 
 export default function Home() {
  return (
