ffaef2cf025ee5116c0d5e8c159c74833ca7a379
cecilieol <cecilieolsen14@gmail.com>
2022-05-29 13:33:13 +0200

    Update SearchResults.js

--- src/components/home/SearchResults.js
+++ src/components/home/SearchResults.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext} from "react";
+import AuthContext from "../../context/AuthContext";
 import { api, hotelEndpoint } from "../../constants/api";
 import axios from 'axios';
 import searchicon from "../../search-interface-symbol.png";
@@ -13,7 +14,10 @@ export default function SearchResults() {
     const [results, setResults] = useState([]);
     const [text, setText] = useState('');
     const [suggestions, setSuggestions] = useState([]);
-    const [isActive, setActive] = useState("false");
+    const [isActive, setActive] = useState(false);
+
+    // eslint-disable-next-line 
+    const [auth, setAuth] = useContext(AuthContext);
 
     useEffect(() => {
         const loadResults = async () => {
@@ -29,12 +33,12 @@ export default function SearchResults() {
         if (text.length > 0) {
             matches = results.filter(result => {
                 const regex = new RegExp(`${text}`, "gi");
+                setActive(true);
                 return result.attributes.name.match(regex);
             })
-            setActive(!isActive);
 
         } else {
-            setActive(isActive);
+            setActive(false);
         }
 
         setSuggestions(matches);
@@ -42,35 +46,35 @@ export default function SearchResults() {
     }
 
     return (
-        <div className="search-container">
-            <div className="search-box">
-            <Heading title="Find your next stay" />
-            <InputGroup className={isActive ? "d-flex m-auto search-bar" : "d-flex m-auto search-bar-active"}>
-                <FormControl
-                  type="search"
-                  placeholder="Search hotel names.."
-                  className="me-2"
-                  aria-label="Search"
-                  onChange={e => onChangeHandler(e.target.value)}
-                  value={text}
-                />
+        <div className={auth ? ("search-container admin") : ("search-container")}>
+            <div className={auth ? ("search-box admin") : ("search-box")}>
+                <Heading title="Find your next stay" />
+                <InputGroup className={isActive ? "d-flex m-auto search-bar-active" : "d-flex m-auto search-bar"}>
+                    <FormControl
+                    type="search"
+                    placeholder="Search hotel names.."
+                    className="me-2"
+                    aria-label="Search"
+                    onChange={e => onChangeHandler(e.target.value)}
+                    value={text}
+                    />
                 
-                <Button variant="link">
-                    <img src={searchicon} alt="Search" className="search-icon" />
-                </Button>
-            </InputGroup>
+                    <Button variant="link">
+                        <img src={searchicon} alt="Search" className="search-icon" />
+                    </Button>
+                </InputGroup>
 
-            {suggestions && suggestions.map((suggestion, i) =>
-            <Link to={`hotels/details/${suggestion.id}`} key={suggestion.id} >
-                <div key={i} className="result">
-                    <img src={suggestion.attributes.image_url_1} alt={suggestion.attributes.name}/>
-                    <div className="d-block ms-2">
-                        <p>{suggestion.attributes.name}</p>
-                        <p>${suggestion.attributes.price} / night</p>
+                {suggestions && suggestions.map((suggestion, i) =>
+                <Link to={`hotels/details/${suggestion.id}`} key={suggestion.id} >
+                    <div key={i} className="result">
+                        <img src={suggestion.attributes.image_url_1} alt={suggestion.attributes.name}/>
+                        <div className="d-block ms-2">
+                            <p>{suggestion.attributes.name}</p>
+                            <p>${suggestion.attributes.price} / night</p>
+                        </div>
                     </div>
-                </div>
-            </Link>
-            )}
+                </Link>
+                )}
             </div>
         </div>
     )
