1425ba5c5571d71b13af4091ab2d197937d56964
cecilieol <cecilieolsen14@gmail.com>
2022-05-29 13:32:52 +0200

    Update Details.js

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -7,6 +7,7 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import EnquiryModal from "./Modal";
+import Loader from "../layout/Loader";
 
 export default function ViewPage() {
 	const [page, setPage] = useState(null);
@@ -38,10 +39,9 @@ export default function ViewPage() {
 		[]
 	);
 
+	if (fetchingPost) return <Loader />;
 
-	if (fetchingPost) return <div>Loading...</div>;
-
-	if (fetchError) return <div>Error loading post</div>;
+	if (fetchError) return <div>An error occured: {fetchError}</div>;
 
 	return (
 		<>
@@ -80,38 +80,39 @@ export default function ViewPage() {
 			</Carousel>
 
 			<Container fluid className="details-container">
-			<Container fluid className="desktop-flex-between p-0">
-			<Row className="page-left">
-				<h1 className="h1-align-left">{page.data.attributes.name}</h1>
-				<p>{page.data.attributes.description}</p>
-			</Row>
-			<Card className="page-right">
-				<div className="card-inline">
-					<Card.Text>Price per night: </Card.Text>
-					<Card.Text>${page.data.attributes.price}</Card.Text>
-				</div>
-				<div className="card-inline">
-					<Card.Text>Max # of guests: </Card.Text>
-					<Card.Text>{page.data.attributes.guests}</Card.Text>
-				</div>
-			<EnquiryModal />
-			</Card>
-			</Container>
+				<Container fluid className="desktop-flex-between p-0">
+					<Row className="page-left">
+						<h1 className="h1-align-left">{page.data.attributes.name}</h1>
+						<p>{page.data.attributes.description}</p>
+					</Row>
 
-			<h2 className="h2-details">Facilities</h2>
-				<ul className="facilities">
-					<li><i className="fa fa-wifi"></i> Wi-Fi: {page.data.attributes.wifi}</li>
-					<li><i className="fa fa-utensils"></i> Breakfast included: {page.data.attributes.breakfast_included}</li>
-					<li><i className="fa fa-bath"></i> Private bathroom: {page.data.attributes.private_bathroom}</li>
-					<li><i className="fa fa-wheelchair"></i> Disability friendly: {page.data.attributes.disability_friendly}</li>
-					<li><i className="fa fa-paw"></i> Pet friendly: {page.data.attributes.pet_friendly}</li>
-					<li><i className="fa fa-umbrella"></i> Complimentary Holidaze umbrellas: Yes</li>
-				</ul>
-			<h2 className="h2-details">Location</h2>
-			<p>{page.data.attributes.address}</p>
-			<div className="view-location">
-    			<Location />
-			</div>
+					<Card className="page-right">
+						<div className="card-inline">
+							<Card.Text>Price per night: </Card.Text>
+							<Card.Text>${page.data.attributes.price}</Card.Text>
+						</div>
+						<div className="card-inline">
+							<Card.Text>Max # of guests: </Card.Text>
+							<Card.Text>{page.data.attributes.guests}</Card.Text>
+						</div>
+						<EnquiryModal />
+					</Card>
+				</Container>
+
+				<h2 className="h2-details">Facilities</h2>
+					<ul className="facilities">
+						<li><i className="fa fa-wifi"></i> Wi-Fi: {page.data.attributes.wifi}</li>
+						<li><i className="fa fa-coffee"></i> Breakfast included: {page.data.attributes.breakfast_included}</li>
+						<li><i className="fa fa-bath"></i> Private bathroom: {page.data.attributes.private_bathroom}</li>
+						<li><i className="fa fa-wheelchair"></i> Disability friendly: {page.data.attributes.disability_friendly}</li>
+						<li><i className="fa fa-paw"></i> Pet friendly: {page.data.attributes.pet_friendly}</li>
+						<li><i className="fa fa-umbrella"></i> Complimentary Holidaze umbrellas: Yes</li>
+					</ul>
+				<h2 className="h2-details">Location</h2>
+					<p>{page.data.attributes.address}</p>
+				<div className="view-location">
+    				<Location />
+				</div>
 			</Container>
         </>
 	);
